d9e3242d3bea65b840e9d094bf4d43e60570d078
André Landim Pereira <46598452+alandim@users.noreply.github.com>
2025-04-18 23:38:52 -0300

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "Criando as imagens....."
 
 docker build -t andrelandim/projeto-backend:1.0 backend/.
@@ -7,3 +9,14 @@ echo "Realizando o push das imagens....."
 
 docker push andrelandim/projeto-backend:1.0
 docker push andrelandim/projeto-database:1.0
+
+echo "Criando serviços no cluster kubernetes....."
+
+kubctl apply -f ./services.yml
+
+echo "Criando os deployments....."
+
+kubctl apply -f ./deployment.yml
+
+
+
